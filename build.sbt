3e77db8f1c83978e004175bcb25ec5da72af7966
Paul Phillips <paulp@improving.org>
2016-06-30 13:47:32 -0600

    Refinements.

--- build.sbt
+++ build.sbt
@@ -1,15 +1,33 @@
 import psp.Sbtx._
 
-                       name := "psp-template"
-                description := "description"
-               organization := "org.improving"
-                   licenses := Seq("Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0"))
-               scalaVersion := "2.11.8"
-         crossScalaVersions := Seq(scalaVersion.value, "2.12.0-M4")
- initialCommands in console += "import java.nio.file._, psp._"
-                shellPrompt := (s => "%s#%s>".format(name.value, Project extract s currentRef))
-                  maxErrors := 15
-           triggeredMessage := Watched.clearWhenTriggered
-        libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test"
+                name := "psp-template"
+         description := "description"
+        organization := "org.improving"
+        scalaVersion := "2.11.8"
+  crossScalaVersions := Seq(scalaVersion.value, "2.12.0-M5")
+            licenses := Seq("Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0"))
+           maxErrors := 15
+         shellPrompt := (s => "%s#%s>".format(name.value, Project extract s currentRef))
+    triggeredMessage := Watched.clearWhenTriggered
+ libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test
 
-inConfig(Test)(testSettings)
+
+inTest(
+   parallelExecution := false,
+     publishArtifact := false,
+         logBuffered := false,
+          traceLevel := 30,
+         testOptions += Tests.Argument(TestFrameworks.ScalaCheck, "-verbosity", "1"),
+         testOptions += Tests.Argument(TestFrameworks.JUnit, envOr("JUNIT", "-a -v -s"): _*)
+)
+
+inBoth(c =>
+  Seq(
+                 scalacOptions in c  :=  envOr("SCALAC", "-language:_ -Yno-adapted-args -Ywarn-unused -Ywarn-unused-import"),
+                  javacOptions in c  :=  envOr("JAVAC", "-nowarn -XDignore.symbol.file"),
+      scalacOptions in console in c  :=  envOr("REPLARGS", "-language:_ -Yno-adapted-args"),
+    initialCommands in console in c  +=  "import java.nio.file._, psp._",
+                        target in c <<=  crossJvmTarget(c),
+    unmanagedSourceDirectories in c <++= crossScalaSource(c)
+  )
+)
--- project/Psp.scala
+++ project/Psp.scala
@@ -6,25 +6,17 @@ object Sbtx {
   type SettingOf[A]   = Def.Initialize[A]
   type TaskOf[A]      = Def.Initialize[Task[A]]
   type InputTaskOf[A] = Def.Initialize[InputTask[A]]
+  type Sets           = Seq[Setting[_]]
+  type Strs           = Seq[String]
 
-  def scalacArgs: Seq[String] = sys.env.getOrElse("SCALAC", "-language:_ -Yno-adapted-args -Ywarn-unused -Ywarn-unused-import").split("\\s+").toSeq
-  def junitArgs: Seq[String]  = sys.env.getOrElse("JUNIT", "-a -v -s").split("\\s+").toSeq
+  def wordSeq(s: String): Strs                = s split "\\s+" filterNot (_ == "") toVector
+  def envOr(key: String, value: String): Strs = wordSeq(sys.env.getOrElse(key, value))
 
-  def testSettings = Seq(
-      parallelExecution := false,
-            logBuffered := false,
-             traceLevel := 30,
-            testOptions += Tests.Argument(TestFrameworks.ScalaCheck, "-verbosity", "1"),
-            testOptions += Tests.Argument(TestFrameworks.JUnit, junitArgs: _*)
-  )
-
-  def wordSeq(s: String): Vector[String] = scala.Vector(s split "\\s+" filterNot (_ == ""): _*)
-
-  def inBoth[A](f: Configuration => Seq[A]): Seq[A] = List(Test, Compile) flatMap f
-
-  def buildBase                   = baseDirectory in ThisBuild
-  def javaSpecVersion: String     = sys.props("java.specification.version")
-  def javaCrossTarget(id: String) = buildBase mapValue (_ / "target" / id / s"java_$javaSpecVersion")
+  def inCompile(ss: Setting[_]*)             = inConfig(Compile)(ss.toSeq)
+  def inTest(ss: Setting[_]*)                = inConfig(Test)(ss.toSeq)
+  def inBoth(f: Configuration => Sets): Sets = List(Test, Compile) flatMap f
+  def buildBase                              = baseDirectory in ThisBuild
+  def javaSpecVersion: String                = sys.props("java.specification.version")
 
   /** Watch out Jonesy! It's the ol' double-cross!
    *  Why, you...
@@ -39,16 +31,22 @@ object Sbtx {
    *
    *  Similarly for main/test, 2.10/2.11, 1.7/1.8.
    */
-  def doubleCross(config: Configuration) = {
-    unmanagedSourceDirectories in config ++= {
-      val jappend = Seq("", "_" + javaSpecVersion)
-      val sappend = Seq("", "_" + scalaBinaryVersion.value)
-      val basis   = (sourceDirectory in config).value
-      val parent  = basis.getParentFile
-      val name    = basis.getName
-      for (j <- jappend ; s <- sappend) yield parent / s"$name$j" / s"scala$s"
-    }
+  def crossScalaSource(config: Configuration) = Def setting {
+    val jappend = Seq("", "_" + javaSpecVersion)
+    val sappend = Seq("", "_" + scalaBinaryVersion.value)
+    val basis   = (sourceDirectory in config).value
+    val parent  = basis.getParentFile
+    val name    = basis.getName
+    for (j <- jappend ; s <- sappend) yield parent / s"$name$j" / s"scala$s"
   }
+
+  /** This separates bytecode by jvm version, so you can switch
+   *  between java versions without it always doing a full recompile.
+   */
+  def crossJvmTarget(c: Configuration) = Def setting {
+    buildBase.value / "target" / thisProject.value.id / s"java_$javaSpecVersion"
+  }
+
   implicit class SettingKeyOps[A](val key: SettingKey[A]) {
     // For a project as obsessed with "map" as is sbt, how did they manage to
     // fuck up calling "map" on a setting? It is implicitly converted to a Task
@@ -60,8 +58,6 @@ object Sbtx {
   }
 
   implicit class ProjectOps(val p: Project) {
-    import p.id
-
     def root: Project = noArtifacts in file(".")
     def noArtifacts: Project = also(
                 publish := (()),
@@ -71,21 +67,7 @@ object Sbtx {
       packagedArtifacts := Map()
     )
     def also(m: ModuleID, ms: ModuleID*): Project     = also(libraryDependencies ++= m +: ms)
-    def deps(ms: ModuleID*): Project                  = also(libraryDependencies ++= ms.toSeq)
     def also(s: Setting[_], ss: Setting[_]*): Project = also(s +: ss.toSeq)
-    def also(ss: Seq[Setting[_]]): Project            = p settings (ss: _*)
-
-    private def crossed = inBoth(c => Seq(scalacOptions in compile in c ++= scalacArgs)) ++ inBoth(doubleCross)
-
-    def setup(): Project = p also crossed also (
-                         name :=  s"psp-$id",
-                 organization :=  "org.improving",
-                 scalaVersion :=  "2.11.8",
-                     licenses :=  Seq("Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
-                       target <<= javaCrossTarget(id),
-      javacOptions in compile ++= Seq("-nowarn", "-XDignore.symbol.file"),
-      publishArtifact in Test :=  false,
-             triggeredMessage :=  Watched.clearWhenTriggered
-    )
+    def also(ss: Sets): Project            = p settings (ss: _*)
   }
 }
